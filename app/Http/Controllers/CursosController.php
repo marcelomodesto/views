0457da362cd601d2a0f6cb0461d2525a34c2efb0
php 35 <php.35@metralha.cec.ime.usp.br>
2017-02-14 20:26:47 -0200

    correcao dos exercicios de views

--- app/Http/Controllers/CursosController.php
+++ app/Http/Controllers/CursosController.php
@@ -7,36 +7,25 @@ use Illuminate\Http\Request;
 class CursosController extends Controller
 {
 
-public $cursos;
+
+
+  public $cursos = [
+   'Lógica de programação',
+   'Desenvolvimento Web com PHP',
+   'Algoritmos e estrutura de dados',
+   'Test Driven Development'
+  ];
+
+
 
   public function index()
     {
-          $this->cursos = array(
-            'Lógica de programação', 'Desenvolvimento Web com PHP',
-            'Algoritmos e estrutura de dados', 'Test Driven Development'
-          );
-
-
-  /*    $cursos = array(
-        'Lógica de programação', 'Desenvolvimento Web com PHP',
-        'Algoritmos e estrutura de dados', 'Test Driven Development'
-      );
-*/
       $cursos = $this->cursos;
       return view('cursos.index', compact('cursos'));
-
     }
 
-
     public function show($index) {
-/*
-      $cursos = array(
-        'Lógica de programação', 'Desenvolvimento Web com PHP',
-        'Algoritmos e estrutura de dados', 'Test Driven Development'
-      );
-*/
       $cursos = $this->cursos;
-    //  $curso = $this->cursos[$index];
       $curso = $cursos[$index];
       return view('cursos.show', compact('curso'));
     }
